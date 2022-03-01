c8907b81634cc7acf3d5577e614c3c07085a059a
student <rmondragon@madisoncollege.edu>
2022-03-01 11:52:37 -0600

    Week7 lab

---
+++ dictionaries.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+myDict = {"server1":"192.168.1.10","server2":"192.168.1.11","server3":"192.168.1.12","server4":"192.168.1.13","server5":"192.168.1.14","server6":"192.168.1.15","server7":"192.168.1.16","server8":"192.168.1.17"}
+
+print(myDict.keys())
+print("\n")
+print(myDict.values())
+print("\n")
+print(myDict.items())
+print("\n")
+
+print('server2' in myDict)
+
+print('server10' in myDict)
+
